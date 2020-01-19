5f271e3b592c34bc7ef0b04e43e0e2458546896a
Koki Takahashi <hakatasiloving@gmail.com>
2020-01-20 02:38:36 +0900

    Fetch data from dynamodb

--- handler.ts
+++ handler.ts
@@ -1,12 +1,26 @@
 /* eslint-disable import/prefer-default-export */
 
+// eslint-disable-next-line no-unused-vars
 import {APIGatewayProxyHandler} from 'aws-lambda';
 import 'source-map-support/register.js';
+import {DynamoDB} from 'aws-sdk';
 
-export const postSession: APIGatewayProxyHandler = async (event, _context) => ({
-	statusCode: 200,
-	body: JSON.stringify({
-		message: 'Yay!',
-		input: event,
-	}, null, 2),
-});
+const db = new DynamoDB.DocumentClient();
+
+export const postSession: APIGatewayProxyHandler = async (event) => {
+	const data = await db.get({
+		TableName: 'hakataarchive-sessions',
+		Key: {
+			id: 'pixiv',
+		},
+	}).promise();
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+			message: 'Yay!',
+			input: event,
+			data: data.Item,
+		}, null, 2),
+	};
+};
