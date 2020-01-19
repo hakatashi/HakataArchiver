e3616a468565ff5c2873f3838cd9510663c7ac07
Koki Takahashi <hakatasiloving@gmail.com>
2020-01-20 03:29:22 +0900

    Authorize with api key

--- handler.ts
+++ handler.ts
@@ -8,19 +8,25 @@ import {DynamoDB} from 'aws-sdk';
 const db = new DynamoDB.DocumentClient();
 
 export const postSession: APIGatewayProxyHandler = async (event) => {
-	const data = await db.get({
+	const body = JSON.parse(event.body);
+
+	if (typeof body.apikey !== 'string' || body.apikey !== process.env.HAKATASHI_API_KEY) {
+		return {
+			statusCode: 403,
+			body: 'apikey is missing or wrong',
+		};
+	}
+
+	await db.put({
 		TableName: 'hakataarchive-sessions',
-		Key: {
-			id: 'pixiv',
+		Item: {
+			id: body.id,
+			session: body.session,
 		},
 	}).promise();
 
 	return {
 		statusCode: 200,
-		body: JSON.stringify({
-			message: 'Pohe!',
-			input: event,
-			data: data.Item,
-		}, null, 2),
+		body: 'ok',
 	};
 };
