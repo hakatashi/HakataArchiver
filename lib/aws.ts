88111eb040ba99cc4973b99092ad7725393e3ee8
Koki Takahashi <hakatasiloving@gmail.com>
2022-02-01 07:59:34 +0900

    Add incrementCounter method in lib/aws

--- lib/aws.ts
+++ lib/aws.ts
@@ -1,4 +1,25 @@
-import {DynamoDB, S3} from 'aws-sdk';
+import {DynamoDB, S3, CloudWatch} from 'aws-sdk';
 
 export const db = new DynamoDB.DocumentClient({convertEmptyValues: true});
 export const s3 = new S3();
+const cloudwatch = new CloudWatch();
+
+export const incrementCounter = (name: string, delta: number = 1) => (
+	cloudwatch.putMetricData({
+		Namespace: 'HakataArchiverMetrics',
+		MetricData: [
+			{
+				MetricName: name,
+				Dimensions: [
+					{
+						Name: 'Application',
+						Value: 'HakataArchive',
+					},
+				],
+				Timestamp: new Date(),
+				Unit: 'Count',
+				Value: delta,
+			},
+		],
+	}).promise()
+);
